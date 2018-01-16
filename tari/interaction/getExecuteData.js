3a4b71455b20f2147e2a6627ffe5ed135f27b33b
Horacio <horacio.gomez@coinfabrik.com>
2018-01-16 13:42:58 -0300

    Added submit transaction script.
    Also fixed data field in transaction.

--- tari/interaction/getExecuteData.js
+++ tari/interaction/getExecuteData.js
@@ -11,7 +11,7 @@ const ms_address = "0x47fEfe4eDe7Cfc136659D3e3098A5FD1B8D99936";
 
 // Construct interface of the wallet contract instance
 const ms_abi = require("../deployment/MultiSigWallet.abi.js");
-const ms_interface = eth.contract(ms_abi);
+const ms_interface = web3.eth.contract(ms_abi);
 const ms_instance = ms_interface.at(ms_address);
 
 // Id of the transaction that is to be confirmed
@@ -22,7 +22,7 @@ const parameters = [transaction_id];
 // Confirm transaction in the multisig;
 // additional signatures may be needed before the transaction is executed
 // If the transaction execution fails, it can be made to execute again calling executeTransaction
-// parameters.push({from: sender_account, data: "0x" + ms_bytecode, gasPrice: 5000000000, gas: 300000});
+// parameters.push({from: sender_account, gasPrice: 5000000000, gas: 300000});
 // myInstance.submitTransaction.apply(parameters);
 const confirm_data = ms_instance.confirmTransaction.getData.apply(parameters);
 
--- tari/interaction/transfer.js
+++ tari/interaction/transfer.js
@@ -1,9 +1,8 @@
 const Web3 = require("web3");
-// const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const web3 = new Web3();
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 // This is the account that submits the transaction
-// const sender_account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc";
+const sender_account = "0x0F048ff7dE76B83fDC14912246AC4da5FA755cFE";
 
 // This is the address of the multisig contract
 // Not actually necessary when encoding parameters according to the ABI
@@ -26,8 +25,6 @@ const parameters = [target_address, value, txn_data];
 // Submit transaction to the multisig;
 // additional signatures may be needed before the transaction is executed
 // If the transaction execution fails, it can be made to execute again calling executeTransaction
-// parameters.push({from: sender_account, gasPrice: 5000000000, gas: 300000});
-// ms_instance.submitTransaction.apply(parameters);
-const transfer_data = ms_instance.submitTransaction.getData.apply(parameters);
+parameters.push({from: sender_account, gasPrice: 41000000000, gas: 300000});
+ms_instance.submitTransaction.apply(parameters);
 
-console.log("Transfer data: " + transfer_data.toString());
