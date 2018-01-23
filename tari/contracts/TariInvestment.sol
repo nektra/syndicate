06c50c1d42089fb90aa59ab1007e1390fc21b836
Sebastián Nale <sebastian.nale@nektra.com>
2018-01-23 10:39:24 -0300

    Changed styling of variables in TariInvestment.

--- tari/contracts/TariInvestment.sol
+++ tari/contracts/TariInvestment.sol
@@ -6,19 +6,19 @@ import "./Ownable.sol";
 contract TariInvestment is Ownable {
 
   // Address of the target contract
-  address public investmentAddress = 0x33eFC5120D99a63bdF990013ECaBbd6c900803CE;
+  address public investment_address = 0x33eFC5120D99a63bdF990013ECaBbd6c900803CE;
   // Major partner address
-  address public majorPartnerAddress = 0x8f0592bDCeE38774d93bC1fd2c97ee6540385356;
+  address public major_partner_address = 0x8f0592bDCeE38774d93bC1fd2c97ee6540385356;
   // Minor partner address
-  address public minorPartnerAddress = 0xC787C3f6F75D7195361b64318CE019f90507f806;
+  address public minor_partner_address = 0xC787C3f6F75D7195361b64318CE019f90507f806;
   // Record balances to allow refunding
   mapping(address => uint) public balances;
   // Total received. Used for refunding.
-  uint public totalInvestment;
+  uint public total_investment;
   // Available refunds. Used for refunding.
-  uint public availableRefunds;
+  uint public available_refunds;
   // Deadline when refunding starts.
-  uint public refundingDeadline;
+  uint public refunding_deadline;
   // Gas used for withdrawals.
   uint public withdrawal_gas;
   // States: Open for investments - allows investments and transfers,
@@ -29,7 +29,7 @@ contract TariInvestment is Ownable {
   State public state = State.Open;
 
   function TariInvestment() public {
-    refundingDeadline = now + 4 days;
+    refunding_deadline = now + 4 days;
     // Withdrawal gas is added to the 2300 call stipend by the EVM (if it has non-zero value).
     set_withdrawal_gas(1000);
   }
@@ -39,7 +39,7 @@ contract TariInvestment is Ownable {
     // Reject any value transfers once refunding stage has been entered.
     require(state == State.Open);
     balances[msg.sender] += msg.value;
-    totalInvestment += msg.value;
+    total_investment += msg.value;
   }
 
   // Transfer some funds to the target investment address.
@@ -53,11 +53,11 @@ contract TariInvestment is Ownable {
     uint minor_fee = transfer_amount * 10 / 1000;
 
     // These calls give no opportunity of reentrancy as long as the owner is not a contract.
-    require(majorPartnerAddress.call.gas(gas_amount).value(major_fee)());
-    require(minorPartnerAddress.call.gas(gas_amount).value(minor_fee)());
+    require(major_partner_address.call.gas(gas_amount).value(major_fee)());
+    require(minor_partner_address.call.gas(gas_amount).value(minor_fee)());
 
     // Send the rest
-    require(investmentAddress.call.gas(gas_amount).value(transfer_amount - major_fee - minor_fee)());
+    require(investment_address.call.gas(gas_amount).value(transfer_amount - major_fee - minor_fee)());
   }
 
   // Convenience function to transfer all available balance.
@@ -70,12 +70,12 @@ contract TariInvestment is Ownable {
   function withdraw() public {
     // Ensure refunding state can be reached if there is no owner intervention.
     if (state != State.Refunding) {
-      require(refundingDeadline <= now);
+      require(refunding_deadline <= now);
       enable_refunds_internal();
     }
 
-    // withdrawal = availableRefunds * investor's share
-    uint withdrawal = availableRefunds * balances[msg.sender] / totalInvestment;
+    // withdrawal = available_refunds * investor's share
+    uint withdrawal = available_refunds * balances[msg.sender] / total_investment;
     balances[msg.sender] = 0;
     // Call is made at the end to ensure observable state of the contract is final
     require(msg.sender.call.gas(withdrawal_gas).value(withdrawal)());
@@ -89,7 +89,7 @@ contract TariInvestment is Ownable {
   // Implements transition to refunding state.
   function enable_refunds_internal() private {
     state = State.Refunding;
-    availableRefunds = this.balance;
+    available_refunds = this.balance;
   }
 
   // Sets the amount of gas allowed to withdrawers
