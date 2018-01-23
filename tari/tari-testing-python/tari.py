d98620dcc6e44d1d210406e6f4a541a628073ef7
Horacio <horacio.gomez@coinfabrik.com>
2018-01-23 16:29:15 -0300

    added logging information and fixed error

--- tari/tari-testing-python/tari.py
+++ tari/tari-testing-python/tari.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3 -i
 from web3 import Web3, HTTPProvider
+import sys
 import json
 import time
 
@@ -23,6 +24,8 @@ def trans(from_index=0, value=0):
 
 dHash = contract.deploy(transaction=trans(0,0))
 
+#name_and_gas = lambda tx_hash : print(sys._getframe().f_code.co_name + ":", gas(tx_hash))
+
 def receipt(tx_hash):
   return eth.getTransactionReceipt(tx_hash)
 
@@ -30,7 +33,7 @@ def status(tx_hash):
   print("Status:", eth.getTransactionReceipt(tx_hash)["status"])
 
 def gas(tx_hash):
-  print("Gas:", eth.getTransactionReceipt(tx_hash)["gasUsed"])
+  return eth.getTransactionReceipt(tx_hash)["gasUsed"]
 
 def addAddress():
   time.sleep(2)
@@ -39,35 +42,54 @@ def addAddress():
   contract.address = receipt(dHash).contractAddress
   print(contract_name, contract.address)
 
-addAddress()
+def balance(address_or_index):
+  if isinstance(address_or_index, str):
+    return eth.getBalance(address_or_index)
+  else:
+    return eth.getBalance(eth.accounts[address_or_index])
 
 def buy(buyer_index, value):
   tx_hash = eth.sendTransaction({"from": eth.accounts[buyer_index], "value": value*(10**18), "gas": 200000, "gasPrice": 0, "to": contract.address})
   time.sleep(2)
-  gas(tx_hash)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
+  
 
-def transfer(amount, gas):
-  tx_hash = contract.transact(trans()).execute_transfer(amount, gas)
+def transfer(amount, gas_amount):
+  tx_hash = contract.transact(trans()).execute_transfer(amount, gas_amount)
   time.sleep(2)
-  gas(tx_hash)
-
-def transfer_all(gas):
-  tx_hash = contract.transact(trans()).execute_transfer_all(gas)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
+  
+def transfer_all(gas_amount):
+  tx_hash = contract.transact(trans()).execute_transfer_all(gas_amount)
   time.sleep(2)
-  gas(tx_hash)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
 
 def withdraw(withdrawer_index):
   tx_hash = contract.transact(trans(withdrawer_index, 0)).withdraw()
   time.sleep(2)
-  gas(tx_hash)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
+  
 
 def enable_refunds():
   tx_hash = contract.transact(trans()).enable_refunds()
   time.sleep(2)
-  gas(tx_hash)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
+  
 
-def set_withdrawal_gas(gas):
-  tx_hash = contract.transact(trans()).set_withdrawal_gas(gas)
+def set_withdrawal_gas(gas_amount):
+  tx_hash = contract.transact(trans()).set_withdrawal_gas(gas_amount)
   time.sleep(2)
-  gas(tx_hash)
+  print(sys._getframe().f_code.co_name, gas(tx_hash))
+  
 
+def ideal_lifecycle():
+
+  buy(1,1000)
+  transfer_all(3000)
+  buy(2,5000)
+  buy(3,3000)
+  buy(4,7000)
+  transfer_all(3000)
+  
+addAddress()
+ideal_lifecycle()
