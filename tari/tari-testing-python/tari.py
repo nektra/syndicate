0bd501dc9d5b18a43f09577c3e368b9d7ba6a3fd
Horacio <horacio.gomez@coinfabrik.com>
2018-01-24 15:13:58 -0300

    test for the Tari remake

--- tari/tari-testing-python/tari.py
+++ tari/tari-testing-python/tari.py
@@ -75,129 +75,24 @@ def partners_balances():
     bals.append(balance(part))
   return bals
 
-def transfer(amount, gas_amount):
-  tx_hash = contract.transact(trans()).execute_transfer(amount*(10**18), gas_amount)
-  time.sleep(2)
-  print(sys._getframe().f_code.co_name, gas(tx_hash))
-  
-def transfer_all(gas_amount):
-  tx_hash = contract.transact(trans()).execute_transfer_all(gas_amount)
+def set_transfer_gas(gas_amount):
+  tx_hash = contract.transact(trans()).set_transfer_gas(gas_amount)
   time.sleep(2)
   print(sys._getframe().f_code.co_name, gas(tx_hash))
 
-def withdraw(withdrawer_index):
-  tx_hash = contract.transact(trans(withdrawer_index, 0)).withdraw()
-  time.sleep(2)
-  print(sys._getframe().f_code.co_name, gas(tx_hash))
-  
-
-def enable_refunds():
-  tx_hash = contract.transact(trans()).enable_refunds()
-  time.sleep(2)
-  print(sys._getframe().f_code.co_name, gas(tx_hash))
-  
-
-def set_withdrawal_gas(gas_amount):
-  tx_hash = contract.transact(trans()).set_withdrawal_gas(gas_amount)
-  time.sleep(2)
-  print(sys._getframe().f_code.co_name, gas(tx_hash))
-  
-
-
-def ideal_lifecycle():
-  init_bals = balances()
-  contract_bal_i = balance(contract.address)
-  partners_bals_i = partners_balances()
-  buy(1,1000)  
-  transfer(100,3000)
-  buy(2,1000)
-  buy(3,2000)
-  transfer_all(3000) 
-  last_bals = balances()
-  print([x-y for x,y in zip(init_bals,last_bals)])  
-  contract_bal_f = balance(contract.address)
-  print("Contract balance difference:", contract_bal_i - contract_bal_f)
-  partners_bals_f = partners_balances()
-  print("Partners balances differences", [x-y for x,y in zip(partners_bals_i,partners_bals_f)])
-
-
-def partial_auto_refund():
-  init_bals = balances()
-  contract_bal_i = balance(contract.address)
-  partners_bals_i = partners_balances()
-  buy(1,1000)  
-  transfer(1000,3000)
-  buy(2,1000)
-  buy(3,2000)
-  time.sleep(20)
-  withdraw(2)
-  withdraw(3)
-  withdraw(1)
-  last_bals = balances()
-  contract_bal_f = balance(contract.address)
-  print("Contract balance difference:", contract_bal_i - contract_bal_f)
-  print([x-y for x,y in zip(init_bals,last_bals)])  
-  partners_bals_f = partners_balances()
-  print("Partners balances differences", [x-y for x,y in zip(partners_bals_i,partners_bals_f)])
-
-
-def manual_refund_total():
+def lifecycle():
   init_bals = balances()
   contract_bal_i = balance(contract.address)
   partners_bals_i = partners_balances()
   buy(1,1000)  
   buy(2,1000)
   buy(3,2000)
-  enable_refunds()
-  withdraw(2)
-  withdraw(3)
-  withdraw(1)
-  last_bals = balances()
-  print([x-y for x,y in zip(init_bals,last_bals)])  
-  contract_bal_f = balance(contract.address)
-  print("Contract balance difference:", contract_bal_i - contract_bal_f)
-  partners_bals_f = partners_balances()
-  print("Partners balances differences", [x-y for x,y in zip(partners_bals_i,partners_bals_f)])
-
-
-def auto_refund_total():
-  init_bals = balances()
-  contract_bal_i = balance(contract.address)
-  partners_bals_i = partners_balances()
-  buy(1,1000)  
-  buy(2,1000)
-  buy(3,2000)
-  time.sleep(20)
-  withdraw(2)
-  withdraw(3)
-  withdraw(1)
-  last_bals = balances()
-  print([x-y for x,y in zip(init_bals,last_bals)])  
-  contract_bal_f = balance(contract.address)
-  print("Contract balance difference:", contract_bal_i - contract_bal_f)
-  partners_bals_f = partners_balances()
-  print("Partners balances differences", [x-y for x,y in zip(partners_bals_i,partners_bals_f)])
-
-
-def partial_manual_refund():
-  init_bals = balances()
-  contract_bal_i = balance(contract.address)
-  partners_bals_i = partners_balances()
-  buy(1,1000)  
-  transfer(1000,3000)
-  buy(2,1000)
-  buy(3,2000)
-  enable_refunds()
-  withdraw(2)
-  withdraw(3)
-  withdraw(1)
   last_bals = balances()
   contract_bal_f = balance(contract.address)
-  print("Contract balance difference:", contract_bal_i - contract_bal_f)
-  print([x-y for x,y in zip(init_bals,last_bals)])  
   partners_bals_f = partners_balances()
-  print("Partners balances differences", [x-y for x,y in zip(partners_bals_i,partners_bals_f)])
-
+  print("Investors balances differences:", [x-y for x, y in zip(init_bals,last_bals)])  
+  print("Contract balance difference:", contract_bal_i - contract_bal_f)
+  print("Partners balances differences", [x-y for x, y in zip(partners_bals_i, partners_bals_f)])
 
 addAddress()
 
